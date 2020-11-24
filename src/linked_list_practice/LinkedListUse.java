f9244943fdbcf654cf723d631e0ddfadfb9b73e0
nigamnayak99 <nigamnayak99@gmail.com>
2020-11-24 12:59:50 +0530

    comments added

--- src/linked_list_practice/LinkedListUse.java
+++ src/linked_list_practice/LinkedListUse.java
@@ -2,17 +2,21 @@ package linked_list_practice;
 import java.util.*;
 
 public class LinkedListUse {
+	/*this function takes input as node data 
+	 * until user puts -1
+	 * */
 	
 	public static Node<Integer> takeInput() {
-		Node<Integer> head = null;
+		Node<Integer> head = null;//answer is null at first
 		Scanner s = new Scanner(System.in);
-		int data = s.nextInt();
-		while(data !=  -1) {
-			Node<Integer> newNode = new Node<Integer>(data);
-			if(head == null) {
-				head = newNode;
+		int data = s.nextInt();// taking first number
+		while(data !=  -1) {//while data is not -1
+			Node<Integer> newNode = new Node<Integer>(data);//create a newNode with given data
+			if(head == null) {//if head is null then it will be first element in ll
+				head = newNode;//so head will be new node and its next reference is automaticallu null
 			}
 			else {
+				//in case it is not the first data in ll
 				Node<Integer> temp = head;
 				while(temp.next != null) {
 					temp = temp.next;
